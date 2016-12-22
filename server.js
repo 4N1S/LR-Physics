45fbd2c7bbcb18478779ad5e1ce62b08e8540f0e
4N1S <anis@crossnode.io>
2016-12-22 03:39:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var port = process.env.PORT || 3000;
 server.listen(port, function() {
   console.log('Listening on ' + port);
 });
-scrapDatas();
 // [ Configuration Server web ]
 app.use(express.static(__dirname +  '/views'));
 app.set('views', __dirname + '/views');
@@ -124,6 +123,7 @@ function scrapDatas(){
 
 	});
 }
+scrapDatas();
 
 function chercheDescr(){
 	console.log("chercheDescr cpt : ".blue + cpt);
